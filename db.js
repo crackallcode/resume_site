c77d3f2de55af1d886117b6bb64f512feaf1b137
PhD Security <76850406+crackallcode@users.noreply.github.com>
2024-05-16 06:57:15 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2")
 
 var db = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     database: "resume",
     user: 'root',
     password: 'password'
